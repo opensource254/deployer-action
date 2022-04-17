7c6407bb85669c18836f83ff9085cf3f6cb02a7a
Stanley Masinde <stanmasinde@gmail.com>
2022-04-17 15:26:26 +0300

    added more verbosity

--- dist/index.js
+++ dist/index.js
@@ -2959,67 +2959,21 @@ module.exports = require("util");
 /******/ 	}
 /******/ 	
 /************************************************************************/
-/******/ 	/* webpack/runtime/compat get default export */
-/******/ 	(() => {
-/******/ 		// getDefaultExport function for compatibility with non-harmony modules
-/******/ 		__nccwpck_require__.n = (module) => {
-/******/ 			var getter = module && module.__esModule ?
-/******/ 				() => (module['default']) :
-/******/ 				() => (module);
-/******/ 			__nccwpck_require__.d(getter, { a: getter });
-/******/ 			return getter;
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/define property getters */
-/******/ 	(() => {
-/******/ 		// define getter functions for harmony exports
-/******/ 		__nccwpck_require__.d = (exports, definition) => {
-/******/ 			for(var key in definition) {
-/******/ 				if(__nccwpck_require__.o(definition, key) && !__nccwpck_require__.o(exports, key)) {
-/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
-/******/ 				}
-/******/ 			}
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
-/******/ 	(() => {
-/******/ 		__nccwpck_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
-/******/ 		// define __esModule on exports
-/******/ 		__nccwpck_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
 /******/ 	/* webpack/runtime/compat */
 /******/ 	
 /******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
 /******/ 	
 /************************************************************************/
 var __webpack_exports__ = {};
-// This entry need to be wrapped in an IIFE because it need to be in strict mode.
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
 (() => {
-"use strict";
-__nccwpck_require__.r(__webpack_exports__);
-/* harmony import */ var _actions_core__WEBPACK_IMPORTED_MODULE_0__ = __nccwpck_require__(935);
-/* harmony import */ var _actions_core__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__nccwpck_require__.n(_actions_core__WEBPACK_IMPORTED_MODULE_0__);
-/* harmony import */ var _actions_exec__WEBPACK_IMPORTED_MODULE_1__ = __nccwpck_require__(409);
-/* harmony import */ var _actions_exec__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__nccwpck_require__.n(_actions_exec__WEBPACK_IMPORTED_MODULE_1__);
-
-
+const core = __nccwpck_require__(935);
+const { exec } = __nccwpck_require__(409);
 
 try {
-    const ipAddress = _actions_core__WEBPACK_IMPORTED_MODULE_0___default().getInput('ip-address')
-    const deploymentPath = _actions_core__WEBPACK_IMPORTED_MODULE_0___default().getInput('deployment-path')
-    const sshKey = _actions_core__WEBPACK_IMPORTED_MODULE_0___default().getInput('ssh-key')
+    const ipAddress = core.getInput('ip-address')
+    const deploymentPath = core.getInput('deployment-path')
+    const sshKey = core.getInput('ssh-key')
     const now = new Date()
 
     // Copy the repo using scp
@@ -3027,12 +2981,13 @@ try {
     console.log(`Executing: ${scpCommand}`)
     
     (async () => {
-        await (0,_actions_exec__WEBPACK_IMPORTED_MODULE_1__.exec)(scpCommand)
+        await exec(scpCommand)
     })()
-    _actions_core__WEBPACK_IMPORTED_MODULE_0___default().setOutput('deployment-path', deploymentPath)
-    _actions_core__WEBPACK_IMPORTED_MODULE_0___default().setOutput('deployment-time', now.toISOString())
+    core.setOutput('deployment-path', deploymentPath)
+    core.setOutput('deployment-time', now.toISOString())
 } catch (error) {
-    _actions_core__WEBPACK_IMPORTED_MODULE_0___default().setFailed(error)
+    console.log(error)
+    core.setFailed(`Action failed with error ${error}`);
 }
 })();
 
