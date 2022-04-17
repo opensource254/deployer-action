f1c36d3dde240c11fed1dba618bfb4477b8a0d19
Stanley Masinde <stanmasinde@gmail.com>
2022-04-17 18:46:02 +0300

    added an absolute path to scp.sh

--- dist/index.js
+++ dist/index.js
@@ -2969,22 +2969,33 @@ var __webpack_exports__ = {};
 (() => {
 const core = __nccwpck_require__(935);
 const { exec } = __nccwpck_require__(409);
-
 try {
     const ipAddress = core.getInput('ip-address')
     const deploymentPath = core.getInput('deployment-path')
     const sshKey = core.getInput('ssh-key')
     const usename = core.getInput('usename')
     const now = new Date()
-    
+
     const deploy = async () => {
-        await exec('chmod', ['+x', 'scp.sh'])
-        await exec('./scp.sh', [sshKey, ipAddress, deploymentPath, usename])
+        let output = ''
+        let errorOutput = ''
+        await exec('chmod', ['+x', './scp.sh'])
+        await exec('./scp.sh', [sshKey, ipAddress, deploymentPath, usename], {
+            listeners: {
+                stdout: (data) => {
+                    output += data.toString()
+                },
+                stderr: (data) => {
+                    errorOutput += data.toString()
+                }
+            }
+        })
     }
 
     deploy()
     core.setOutput('deployment-path', deploymentPath)
     core.setOutput('deployment-time', now.toISOString())
+    core.setOutput('deployment-output', output)
 } catch (error) {
     const e = new Error(error)
     core.setFailed(`Action failed with message: ${e.message}`)
--- index.js
+++ index.js
@@ -1,21 +1,32 @@
 const core = require('@actions/core');
 const { exec } = require('@actions/exec');
-
 try {
     const ipAddress = core.getInput('ip-address')
     const deploymentPath = core.getInput('deployment-path')
     const sshKey = core.getInput('ssh-key')
     const usename = core.getInput('usename')
     const now = new Date()
-    
+
     const deploy = async () => {
-        await exec('chmod', ['+x', 'scp.sh'])
-        await exec('./scp.sh', [sshKey, ipAddress, deploymentPath, usename])
+        let output = ''
+        let errorOutput = ''
+        await exec('chmod', ['+x', './scp.sh'])
+        await exec('./scp.sh', [sshKey, ipAddress, deploymentPath, usename], {
+            listeners: {
+                stdout: (data) => {
+                    output += data.toString()
+                },
+                stderr: (data) => {
+                    errorOutput += data.toString()
+                }
+            }
+        })
     }
 
     deploy()
     core.setOutput('deployment-path', deploymentPath)
     core.setOutput('deployment-time', now.toISOString())
+    core.setOutput('deployment-output', output)
 } catch (error) {
     const e = new Error(error)
     core.setFailed(`Action failed with message: ${e.message}`)
